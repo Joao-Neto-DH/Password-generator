f8d77577da3c925d951be15a7f0d78b8fc1c7879
Joao-Neto-DH <joaolima882018@gmail.com>
2022-08-31 20:44:13 +0100

    Criando função para gerar a password

--- assets/generator.js
+++ assets/generator.js
@@ -1,1 +1,21 @@
 "use strict";
+
+
+function generatePassword(max) {
+    const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0];
+    
+    let pass = "";
+    
+    for (let i = 0; i < max; i++) {
+        let index = Math.round(Math.random() * letters.length) % letters.length;
+        // console.log(index);
+        let letter = letters[index];
+        if (Math.random() < 0.4)
+            letter = letter.toString().toUpperCase();
+        pass += letter;    
+    }
+
+    return pass;
+}
+
+console.log(generatePassword(8));
